83777d996d9bfe5283bc3ba62d4780cdbc231282
Kavita <kavitaninave123@gmail.com>
2025-09-03 18:07:50 +0530

    edit in contollers

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -5,13 +5,13 @@ import Category from "../models/category.js";
 const getAllCategories = async (req, res) => {
     try {
         const categories = await Category.find()
-        res.status(200).json({
+       return res.status(200).json({
             success: true,
             categories,
         });
 
     } catch (error) {
-        res.status(500).json({
+       return res.status(500).json({
             success: false,
             message: "Failed to retrieve categories",
             error: err.message,
